1d851e59ad7f93c9e5d075d4eda3b875c9a7e1b6
Anikroy7 <roy645800@gmail.com>
2022-08-26 19:16:30 +0600

    connect to mongodb and products api implement

--- index.js
+++ index.js
@@ -4,10 +4,42 @@ const cors = require('cors');
 
 const port = process.env.PORT || 5000
 require('dotenv').config()
+
 // middleware 
 
 app.use(cors())
 app.use(express.json())
+
+
+// CONNECT TO MONGOBD
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ro5atxz.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect()
+
+        const serviceCollection = client.db('sharePal').collection('product');
+
+        app.get('/products', async (req, res) => {
+
+            const query = {};
+            const result = await serviceCollection.find(query).toArray();
+            console.log(result);
+            res.send(result)
+
+        })
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to our server')
@@ -16,3 +48,11 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Sharepal app listening on port ${port}`)
 })
+
+
+
+
+
+
+
+
